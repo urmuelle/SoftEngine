3359adf2563c4298b91d004185fe2ed8a715203d
Urs Müller <ursmueller@gmx.ch>
2022-05-06 18:54:23 +0200

    Added Mesh Loading Functionality

--- SoftEnginge/MainWindow.xaml.cs
+++ SoftEnginge/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace SoftEngine
     public partial class MainWindow : Window
     {
         private Device device;
-        private Mesh mesh = new Mesh("Cube", 8, 12);
+        private Mesh[] meshes;
         private Camera camera = new ();
 
         public MainWindow()
@@ -43,28 +43,7 @@ namespace SoftEngine
             // Our Image XAML control
             Image1.Source = bmp;
 
-            mesh.Vertices[0] = new Vector3(-1, 1, 1);
-            mesh.Vertices[1] = new Vector3(1, 1, 1);
-            mesh.Vertices[2] = new Vector3(-1, -1, 1);
-            mesh.Vertices[3] = new Vector3(1, -1, 1);
-            mesh.Vertices[4] = new Vector3(-1, 1, -1);
-            mesh.Vertices[5] = new Vector3(1, 1, -1);
-            mesh.Vertices[6] = new Vector3(1, -1, -1);
-            mesh.Vertices[7] = new Vector3(-1, -1, -1);
-
-            mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
-            mesh.Faces[1] = new Face { A = 1, B = 2, C = 3 };
-            mesh.Faces[2] = new Face { A = 1, B = 3, C = 6 };
-            mesh.Faces[3] = new Face { A = 1, B = 5, C = 6 };
-            mesh.Faces[4] = new Face { A = 0, B = 1, C = 4 };
-            mesh.Faces[5] = new Face { A = 1, B = 4, C = 5 };
-
-            mesh.Faces[6] = new Face { A = 2, B = 3, C = 7 };
-            mesh.Faces[7] = new Face { A = 3, B = 6, C = 7 };
-            mesh.Faces[8] = new Face { A = 0, B = 2, C = 7 };
-            mesh.Faces[9] = new Face { A = 0, B = 4, C = 7 };
-            mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
-            mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
+            meshes = device.LoadJSONFileAsync("monkey.babylon");
 
             camera.Position = new Vector3(0, 0, 10.0f);
             camera.Target = Vector3.Zero;
@@ -78,11 +57,14 @@ namespace SoftEngine
         {
             device.Clear(0, 0, 0, 255);
 
-            // rotating slightly the cube during each frame rendered
-            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            foreach (var mesh in meshes)
+            {
+                // rotating slightly the meshes during each frame rendered
+                mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            }
 
             // Doing the various matrix operations
-            device.Render(camera, mesh);
+            device.Render(camera, meshes);
 
             // Flushing the back buffer into the front buffer
             device.Present();
